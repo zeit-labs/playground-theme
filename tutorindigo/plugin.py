6b30a38c96ee359893043a2d633d33ba2fffebd5
Omar Al-Ithawi <i@omardo.com>
2025-02-20 15:21:14 +0300

    fix: fixup previous commit

--- tutorindigo/plugin.py
+++ tutorindigo/plugin.py
@@ -13,7 +13,7 @@ from .__about__ import __version__
 if __version_suffix__:
     __version__ += "-" + __version_suffix__
 
-BRAND_PACKAGE = '@edx/brand@git+https://github.com/zeit-labs/brand-openedx.git#b2f8a6e'
+BRAND_PACKAGE = '@edx/brand@git+https://github.com/zeit-labs/brand-openedx.git#08545d3460a56dfa5e8f2bee44aa5874835187ca'
 
 ################# Configuration
 config: t.Dict[str, t.Dict[str, t.Any]] = {
